3ae5bdcdc058d6e2d54a2237537a5a7d55e80068
Pavel Stupnikov <dp@dpointer.org>
2012-10-11 14:09:23 +0400

    enclosed keys in quotes in object output

---
+++ jinja2js/test_templates/object_definition.js
@@ -0,0 +1,29 @@
+(function(__ns, _) {
+__ns.object_definition = function() {
+    var __data = _.parse_args(arguments, []);
+    var __output = '';
+    var a = {'a': 'b', 'b': 'c'};
+    return __output;
+};
+
+__ns.object_definition_with_varible_value = function() {
+    var __data = _.parse_args(arguments, ['val']);
+    var __output = '';
+    var a = {'a': __data.val, 'b': 'c'};
+    return __output;
+};
+
+__ns.object_definition_with_dot_in_key = function() {
+    var __data = _.parse_args(arguments, []);
+    var __output = '';
+    var a = {'a.b': 'b', 'b': 'c'};
+    return __output;
+};
+
+__ns.object_definition_with_quotes_in_key = function() {
+    var __data = _.parse_args(arguments, []);
+    var __output = '';
+    var a = {"'a.b'": 'b', '"b"': 'c', 'b"\'': 'b"\'', 'b\'"': 'b\'"'};
+    return __output;
+};
+})(window.jinja2js = window.jinja2js || {}, jinja2support);
