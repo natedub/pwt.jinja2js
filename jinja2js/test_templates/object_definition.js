8c9431105176934cac6966c98f664e3fa02f2548
Pavel Stupnikov <dp@dpointer.org>
2012-10-11 14:19:33 +0400

    added test for integer keys

--- jinja2js/test_templates/object_definition.js
+++ jinja2js/test_templates/object_definition.js
@@ -26,4 +26,11 @@ __ns.object_definition_with_quotes_in_key = function() {
     var a = {"'a.b'": 'b', '"b"': 'c', 'b"\'': 'b"\'', 'b\'"': 'b\'"'};
     return __output;
 };
+
+__ns.object_definition_with_numbers_in_key = function() {
+    var __data = _.parse_args(arguments, []);
+    var __output = '';
+    var a = {1: 'b', 3: 'c'};
+    return __output;
+};
 })(window.jinja2js = window.jinja2js || {}, jinja2support);
