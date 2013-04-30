fe24f0e326113e587f40304fc84472bedf0d2915
William Kral <william.kral@gmail.com>
2013-04-29 19:39:53 -0700

    Update version so it will install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "jinja2js",
-    version = "0.7.4",
+    version = "0.8.0",
 
     author = "William Kral, Michael Kerrin",
     author_email = "william.kral@gmail.com, michael.kerrin@gmail.com",
